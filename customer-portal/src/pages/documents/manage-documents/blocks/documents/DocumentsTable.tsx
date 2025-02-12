ae317ec4fab3d95ada1475e805b8e6c8b6658e4b
gixion1010 <121560528+gixion1010@users.noreply.github.com>
2025-02-12 16:26:40 +0500

    Update .gitignore to exclude .env file and rename header in DocumentsTable component

--- customer-portal/src/pages/documents/manage-documents/blocks/documents/DocumentsTable.tsx
+++ customer-portal/src/pages/documents/manage-documents/blocks/documents/DocumentsTable.tsx
@@ -254,7 +254,7 @@ const DocumentsTable = () => {
 
     return (
       <div className="card-header flex-wrap gap-2 border-b-0 px-5">
-        <h3 className="card-title font-medium text-sm">Service Requests</h3>
+        <h3 className="card-title font-medium text-sm">Documents</h3>
 
         <div className="flex flex-wrap gap-2 lg:gap-5">
           <div className="flex">
