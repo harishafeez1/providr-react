a19cf9b496a726963bac487740a6b011e73fb933
Ahmad Abbas <82842965+AhmadAbbas949@users.noreply.github.com>
2025-03-06 20:49:42 +0500

    Update CustomerServiceRequestsTable.tsx

--- provider-portal/src/pages/service-requests/manage-customer-service-requests/blocks/customer-service-requests/CustomerServiceRequestsTable.tsx
+++ provider-portal/src/pages/service-requests/manage-customer-service-requests/blocks/customer-service-requests/CustomerServiceRequestsTable.tsx
@@ -212,7 +212,7 @@ const CustomerServiceRequestsTable = () => {
         cell: (info) => {
           return (
             <div className="flex items-center text-gray-800 font-normal gap-1.5">
-              {`${info.row.original.customer.first_name} ${info.row.original.customer?.last_name || ''}`}
+              {`${info.row.original.customer?.first_name || ''} ${info.row.original.customer?.last_name || ''}`}
             </div>
           );
         },
